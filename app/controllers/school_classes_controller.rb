3600fe74b70416fd4be878b0dfe824570ab7c718
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-12-10 06:48:26 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,30 +1,30 @@
 class SchoolClasses < ApplicationController
 	def index
-		@school_classes = Post.all
+		@school_classes = SchoolClass.all
 	end
 
 	def show
-		@school_class = Post.find(params[:id])
+		@school_class = SchoolClass.find(params[:id])
 	end
 
 	def new
-		@school_class = Post.new
+		@school_class = SchoolClass.new
 	end
 
 	def create
-    @school_class = Post.new(post_params(:title, :description))
+    @school_class = SchoolClass.new(post_params(:title, :description))
 	  @school_class.save
 	  redirect_to post_path(@school_class)
 	end
 
 	def update
-	  @school_class = Post.find(params[:id])
+	  @school_class = SchoolClass.find(params[:id])
     @school_class.update(school_class_params(:title))
 	  redirect_to school_class_path(@school_class)
 	end
 
 	def edit
-	  @school_class = Post.find(params[:id])
+	  @school_class = SchoolClass.find(params[:id])
 	end
 
   private
